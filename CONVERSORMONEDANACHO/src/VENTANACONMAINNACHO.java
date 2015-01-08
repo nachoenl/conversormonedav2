ee9975b0f4d26ade749e45fb0f2a622db414917f
nachoenl <nachoenl@gmail.com>
2015-01-09 00:29:41 +0100

    segundo intento desubir TODO NO SOLO LO QUE LE VENGA EN GANA A GITHUB

---
+++ CONVERSORMONEDANACHO/src/VENTANACONMAINNACHO.java
@@ -0,0 +1,20 @@
+
+public class VENTANACONMAINNACHO {
+
+	//public VENTANACONMAINNACHO() { pasa al otro fichero
+		// TODO Auto-generated constructor stub  =>que es esto exactamente?
+
+	public static void main(String[] args) {
+		//creo un objeto llamado ventanaEurocalculadora, el cual
+		//será del tipo MICLASECONVERSORNACHO , definido en el otro archivo
+		
+	    MICLASECONVERSORNACHO VentanaEurocalculadora1=new MICLASECONVERSORNACHO();
+		VentanaEurocalculadora1.setVisible(true);
+		//si no le aplico el metodo .setvisible visualizar ...todo lo anterior no vale de nada
+	}
+	
+}
+	
+// VentanaEurocalculadora se construye y dibuja a partir de la información del otro .java
+	
+	
